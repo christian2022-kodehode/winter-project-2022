efdd3763a32f821d910e7135361c0ba4a00a903d
ChristianAk <christian2022.kodehode@gmail.com>
2023-01-11 12:49:17 +0100

    Added submit message to localstorage feature.

--- src/components/Compose/Compose.jsx
+++ src/components/Compose/Compose.jsx
@@ -1,12 +1,95 @@
 import React from "react"
 
+// placeholder values
+const username = "Bruker Brukersen"
+const channel = "jul2023"
+const zone = "cet"
+const lang = "nb"
+
 export default function Compose () {
 
-	const [message, setMessage] = React.useState("")
+	const [message, setMessage] = React.useState ("")
 
-	function submitForm(event) {
-		event.preventDefault()
-		console.log("message:", message)
+	function submitForm (event) {
+
+		event.preventDefault ()
+
+		let data = []
+		let channelKey = null
+		let dateKey = null
+		let messageKey = null
+
+		// Check if any message data exist in localStorage
+		if (localStorage.getItem ("messageData")) {
+
+			data = JSON.parse (localStorage.getItem ("messageData"))
+
+			// Check if object for current channel exists
+			for (let channelIndex = 0; channelIndex < data.length; channelIndex++) {
+				if (data[channelIndex].channel === channel) channelKey = data[channelIndex].key
+			}
+
+			if (data[channelKey -1]) {
+
+				// Check if object for current date exists
+				for (let dateIndex = 0; dateIndex < data[channelKey -1].dates.length; dateIndex++) {
+					if (data[channelKey -1].dates[dateIndex].date === new Date().toDateString()) dateKey = data[channelKey -1].dates[dateIndex].key
+				}
+			}
+		}
+
+		if (channelKey === null) {
+
+			// Create new channel in object
+			channelKey = data.length +1
+
+			data.push (
+				{
+				"key": channelKey,
+				"channel": channel,
+				"dates": []
+				}
+			)
+		}
+
+		if (dateKey === null) {
+
+			dateKey = data[channelKey -1].dates.length +1
+
+			// Create new object for current date
+			data[channelKey -1].dates.push (
+				{
+				"key": dateKey,
+				"date": new Date().toDateString(),
+				"messages": []
+				}
+			)
+		}
+
+		if (data[channelKey -1].dates[dateKey -1].messages.length > 0) {
+
+			messageKey = data[channelKey -1].dates[dateKey -1].messages.length +1
+		}
+		else {
+
+			messageKey = 1
+		}
+
+		data[channelKey -1].dates[dateKey -1].messages.push (
+			{
+			"key": messageKey,
+			"time": new Date().toJSON(),
+			"zone": zone,
+			"language": lang,
+			"author": username,
+			"body": message
+			}
+		)
+
+		localStorage.setItem("messageData", JSON.stringify(data))
+
+		// Clear textarea
+		setMessage("")
 	}
 
 	function updateMessage(event) {
@@ -37,7 +120,6 @@ export default function Compose () {
 				</form>
 			</div>
 		</div>
-
 
 	)
 }
