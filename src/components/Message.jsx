6a60261bd65e0a44d792e5887ab0051af491faf9
ChristianAk <christian2022.kodehode@gmail.com>
2023-01-17 13:33:51 +0100

    Fixed some adjustments for time zone.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,6 @@
-export default function Message (props) {
+export default function Message( props ) {
 
+	// Format message timestamp into human-readable string
 	const options = {
 		timeZone: "Europe/Oslo",
 		hour12: false,
@@ -7,18 +8,16 @@ export default function Message (props) {
 		minute: "2-digit",
 		second: "2-digit"
 	}
-
-	const time = new Date(props.message.time).toLocaleTimeString("no-NB", options)
+	const time = new Date( props.message.time ).toLocaleTimeString( "no-NB", options )
 
 	return (
 
 		<div className="message">
-			<p className="message__text">{props.message.body}</p>
+			<p className="message__text">{ props.message.body }</p>
 			<div className="message__byline">
 				<p className="message__timestamp">{ time }</p>
-				<p className="message__author">{props.message.author}</p>
+				<p className="message__author">{ props.message.author }</p>
 			</div>
 		</div>
-
 	)
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,37 +1,43 @@
 import { useState, useEffect } from "react"
 
-export default function Timer (props) {
+export default function Timer( props ) {
 
-	function getTimeRemaining (time = props.channel.time) {
-		const total = Date.parse(time) - Date.parse(new Date())
-		const days		= Math.floor((total / 1000 / 60 / 60 / 24) % 365).toString().padStart(3,0)
-		const hours		= Math.floor((total / 1000 / 60 / 60) % 24).toString().padStart(2,0)
-		const minutes	= Math.floor((total / 1000 / 60) % 60).toString().padStart(2,0)
-		const seconds	= Math.floor((total / 1000) % 60).toString().padStart(2,0)
+	// Take provided time and adjust for user's time zone
+	const offsetTime = ( new Date().getTimezoneOffset() * 1000 * 60 ) + Date.parse( props.channel.time )
+
+	// Format time into a human readable countdown string
+	function getTimeRemaining(time = offsetTime) {
+		const total = time - Date.parse( new Date() )
+		const days		= Math.floor( ( total / 1000 / 60 / 60 / 24 ) % 365 ).toString().padStart( 3,0 )
+		const hours		= Math.floor( ( total / 1000 / 60 / 60 ) % 24 ).toString().padStart( 2,0 )
+		const minutes	= Math.floor( ( total / 1000 / 60 ) % 60 ).toString().padStart( 2,0 )
+		const seconds	= Math.floor( ( total / 1000 ) % 60 ).toString().padStart( 2,0 )
 		const formattedTime = `${days} : ${hours} : ${minutes} : ${seconds}`
 		return formattedTime
 	}
 
-	const [ timer, setTimer ] = useState ( getTimeRemaining () );
+	const [ timer, setTimer ] = useState( getTimeRemaining() )
 
-	useEffect (
+	// Refresh timer every second
+	useEffect(
 		() => {
-			setInterval (() => setTimer ( getTimeRemaining () ), 1000 );
+			setInterval( () => setTimer( getTimeRemaining() ), 1000 )
+			// todo: make timer stop at 0, and trigger end message
 		}
 	)
 
-	const options = {
+	// Format target date into human readable string
+	const dateOptions = {
+		hour12: false,
 		timeZone: "Europe/Oslo",
 		weekday: "long",
 		day: "numeric",
 		month: "long",
 		year: "numeric",
-		hour12: false,
 		hour: "2-digit",
 		minute: "2-digit"
 	}
-
-	const target = new Date(props.channel.time).toLocaleTimeString("no-NB", options)
+	const target = new Date( offsetTime ).toLocaleTimeString( "no-NB", dateOptions )
 
 	return (
 		<div className="container">
@@ -40,7 +46,7 @@ export default function Timer (props) {
 				<div className="countdown__description">{ props.channel.description }</div>
 			</div>
 			<div className="countdown__time">
-				<h2 className="countdown__now">{ timer }</h2>
+				<h2 className="countdown__now">-{ timer }</h2>
 				<div className="countdown__target">{ target }</div>
 			</div>
 		</div>
